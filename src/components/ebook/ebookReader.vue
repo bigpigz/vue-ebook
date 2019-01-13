395ee010656ad535aeab80de354384213b0fc908
bigz <493971184@qq.com>
2019-01-13 19:49:39 +0800

    4-3阅读器翻页

--- src/components/ebook/ebookReader.vue
+++ src/components/ebook/ebookReader.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="ebook-reader">
+        <div class="mask">
+            <div class="left" @click="prevPage()"></div>
+            <div class="center" @click="toggleMenu()"></div>
+            <div class="right" @click="nextPage()"></div>
+        </div>
         <div id="read"></div>
+
     </div>
 </template>
 
@@ -18,18 +24,52 @@
             initEpub(){
                 const url = 'http://localhost:8088/epub/' + this.fileName + '.epub'
                 this.book = new Epub(url)
-                this.rendition = this.book.renderTo('read',{
-                    width:innerWidth,
-                    height:innerHeight,
-                    method:'default'  //兼容微信视图
+                this.rendition = this.book.renderTo('read', {
+                    width: innerWidth,
+                    height: innerHeight,
+                    method: 'default'  //兼容微信视图
                 })
                 this.rendition.display()
+
+                this.rendition.on('touchstart', event => {
+                    this.touchStartX = event.changedTouches[0].clientX
+                    this.touchStartTime = event.timeStamp
+                })
+                this.rendition.on('touchend', event => {
+                    const offsetX = event.changedTouches[0].clientX - this.touchStartX
+                    const time = event.timeStamp - this.touchStartTime
+                    if (time < 500 && offsetX > 40) {
+                        this.prevPage()
+                    } else if (time < 500 && offsetX < -40) {
+                        this.nextPage()
+                    } else {
+                        this.toggleMenuVisible()
+                    }
+                    event.preventDefault()
+                    event.stopPropagation()
+                })
+
+            },
+            prevPage(){
+                if(this.rendition){
+                    this.rendition.prev()
+                }
+            },
+            toggleMenu(){
+                console.log('menu');
+            },
+            nextPage(){
+                if(this.rendition){
+                    this.rendition.next()
+                }
+
             }
         },
         mounted(){
             const fileName = this.$route.params.fileName.split('|').join('/')
-            this.$store.dispatch('setFileName',fileName).then(()=>{
+            this.$store.dispatch('setFileName', fileName).then(() => {
                 this.initEpub()
+
             })
         }
 
@@ -38,4 +78,31 @@
 
 <style lang="scss" scoped type="text/scss">
     @import "../../assets/styles/global";
+    .mask{
+        position: absolute;
+        left: 0;
+        top: 0;
+        overflow: hidden;
+        z-index: 999;
+        height: 100%;
+        width: 100%;
+        .left{
+            float: left;
+            height: 100%;
+            width: 33%;
+            /*background: black;*/
+        }
+        .center{
+            float: left;
+            height: 100%;
+            width: 34%;
+            /*background: skyblue;*/
+        }
+        .right{
+            float: right;
+            height: 100%;
+            width: 33%;
+            /*background: blue;*/
+        }
+    }
 </style>
